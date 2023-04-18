14e28a1ff6607f166d90008c56b43aa578c0f638
10Rup <104366033+10Rup@users.noreply.github.com>
2023-04-18 18:28:20 +0530

    code1

--- Dashboard/app1.py
+++ Dashboard/app1.py
@@ -11,32 +11,87 @@ import dash_bootstrap_components as dbc
 
 from dash import Dash, dcc, html, Input, Output
 
-# print("hello world")
+
 x = bar_gph.Barclass('a', 'b')
 print((bar_gph.long_df["nation"]))
 # x.name = "rup"
 # print(x.method1())
 
+
+# =========================================================
+# style for sidebar
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+
+# ===================================================
+
 app = Dash(__name__)
 
 
-app.layout = html.Div([
+app.layout =html.Div([
 
-    html.Div([
-        dcc.Dropdown(
-            bar_gph.nations,
-            id="drop_1",
-            maxHeight=100,
-            style={'width': '200px'},
+    html.Div(
+        [
+            html.H2("Sidebar", className="display-4"),
+            html.Hr(),
+            html.P(
+                "A simple sidebar layout with navigation links", className="lead"
+            ),
+            dbc.Nav(
+                [
+                    # html.Div([dbc.NavLink("Home", href="/", active="exact")]),
+                    dbc.NavLink("Home", href="/", active="exact"),
+                    dbc.NavLink("Page 1", href="/page-1", active="exact"),
+                    dbc.NavLink("Page 2", href="/page-2", active="exact"),
+                ],
+                vertical=True,
+                pills=True,
+            ),
+        ],
+        style={
+            # "border": "20 20 20 20",
+            # "border-color": "coral",
+            "position": "fixed",
+            "top": 0,
+            "left": 0,
+            "width": "150px",
+            "height": "100%",
+            "padding": "20px 10px",
+            "background-color": "rgb(151 171 194)",
+        },
+    ),
 
-        )
-    ]),
+    # html.Div([
+    #     dcc.Dropdown(
+    #         bar_gph.nations,
+    #         id="drop_1",
+    #         maxHeight=100,
+    #         style={'width': '200px'},
+    #
+    #     )
+    # ]),
 
 
 
-    dcc.Graph(
-        figure=x.method1()
-    )
+    # dcc.Graph(
+    #     figure=x.method1()
+    # )
 
 ])
 
--- Dashboard/bar_graph.py
+++ Dashboard/bar_graph.py
@@ -3,8 +3,9 @@ import plotly.express as px
 
 long_df = px.data.medals_long()
 nations = list(map(str, long_df["nation"].unique()))
-# print(data)
+
 class Barclass:
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -12,4 +13,5 @@ class Barclass:
     def method1(self):
         print(f"Hello {self.name}")
         fig = px.bar(long_df, x="nation", y="count", color="medal", title="Long-Form Input")
-        return fig #fig.show()
+        # fig.show()
+        return fig

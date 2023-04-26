dbfb4c0637be56bbb4183f7a47d9625e7f6c081c
10Rup <104366033+10Rup@users.noreply.github.com>
2023-04-26 19:03:23 +0530

    code5

--- Dashboard/app1.py
+++ Dashboard/app1.py
@@ -21,47 +21,58 @@ external_stylesheet4 = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.
 app = Dash(__name__, external_stylesheets=[external_stylesheet1, external_stylesheet2, external_stylesheet3,
                                            external_stylesheet4])
 
-
 app.layout = html.Div([
     dbc.Row([
-        html.Div([
+        # html.Div([
 
-            dbc.Col([
+        dbc.Col([
 
-                dbc.Button([], className="fa fa-bar-chart", style={
-                    'margin-left': '12px',
-                    'font-size': '20px',
-                    'background-color': 'black', 'border-color': 'transparent',
-                    'color': 'yellow', 'margin-right': '2px',
-                }),
-                dbc.Button([], className="fa fa-area-chart", style={
-                    'font-size': '20px',
-                    'background-color': 'black', 'border-color': 'transparent',
-                    'color': '#8700ff', 'margin-right': '2px',
-                    'margin-left': '1px'
-                }),
-                dbc.Button([], className="fa fa-line-chart", style={
-                    'font-size': '20px',
-                    'background-color': 'black', 'border-color': 'transparent',
-                    'color': '#0dd092', 'margin-right': '2px',
-                }),
-                dbc.Button([], className="fa fa-pie-chart", style={
-                    'font-size': '20px',
-                    'background-color': 'black', 'border-color': 'transparent',
-                    'color': '#ef0fa0', 'margin-right': '1px',
-                }),
-            ], width=2, style={'border': '1px solid', 'padding-top': '2px', 'padding-bottom': '2px'})
-        ], style={'margin-left': '1px'})
-    ])
+            dbc.Button([], id='btn-1', className="fa fa-bar-chart", n_clicks=0,
+                       style={
+                           'margin-left': '0px', 'font-size': '20px',
+                           'background-color': 'black', 'border-color': 'transparent',
+                           'color': 'yellow', 'margin-right': '2px',
+                       }),
+            dbc.Button([], className="fa fa-area-chart", style={
+                'font-size': '20px',
+                'background-color': 'black', 'border-color': 'transparent',
+                'color': '#8700ff', 'margin-right': '2px',
+                'margin-left': '1px'
+            }),
+            dbc.Button([], className="fa fa-line-chart", style={
+                'font-size': '20px',
+                'background-color': 'black', 'border-color': 'transparent',
+                'color': '#0dd092', 'margin-right': '2px',
+            }),
+            dbc.Button([], className="fa fa-pie-chart", style={
+                'font-size': '20px',
+                'background-color': 'black', 'border-color': 'transparent',
+                'color': '#ef0fa0', 'margin-right': '1px',
+            }),
+        ], width=2, style={'height': '150px', 'border': '2px solid',
+                           'padding-top': '2px', 'padding-bottom': '2px'}),
+
+        dbc.Col([
+            html.P(id='out-id', children=['output-'])
+
+        ], width=3, style={'border': '2px solid'}),
+
+    ], style={'margin-left': '1px'})
+
+    # ])
 
 ])
-# @app.callback(
-#     Output('tab1_output', "children"),
-#     [Input("drop1", "value")]
-# )
-# def show_output(n):
-#     if n:
-#         return f'selected values is {n}'
+
+
+@app.callback(
+    Output("out-id", "children"), [Input("btn-1", "n_clicks")]
+)
+def show_output(n):
+    if n is None:
+        return "none"
+    else:
+        return f'output-{n}'
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
